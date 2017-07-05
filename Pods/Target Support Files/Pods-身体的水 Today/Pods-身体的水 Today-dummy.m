822646c907e65e4c1cef1ed09b894da7f355ffb1
白彬涵 <MRWhite@WhiteMac.local>
2017-07-05 10:47:11 +0800

    update .gitignore

--- Pods/Target Support Files/Pods-身体的水 Today/Pods-身体的水 Today-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods______Today : NSObject
-@end
-@implementation PodsDummy_Pods______Today
-@end
--- Pods/Target Support Files/Pods-身体的水/Pods-身体的水-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_____VersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_____VersionString[];
-
